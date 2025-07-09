96a5bf364496bed57864f743be90a20275766d13
Farrah Mae Ochoa <farrah@deriv.com>
2025-07-09 15:23:26 +0400

    fix: auth token

--- lib/local-variables.ts
+++ lib/local-variables.ts
@@ -49,6 +49,7 @@ export const WALLETS = {
 
 export const AUTH = {
   getAuthHeader: () => ({
+    Authorization: `Bearer ${USER_TOKEN}`,
     "X-Data-Source": process.env.NEXT_PUBLIC_DATA_SOURCE,
     "X-Branch": process.env.NEXT_PUBLIC_BRANCH,
     "Content-Type": "application/json",
