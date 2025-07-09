67f45f4b6841899434ef998aada558ce67b18f54
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2025-07-09 10:35:27 +0400

    Revert "farrah/ updated the buy-sell page"

--- app/main.tsx
+++ app/main.tsx
@@ -2,7 +2,7 @@
 
 import type React from "react"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { usePathname, useRouter } from "next/navigation"
 import MobileFooterNav from "@/components/mobile-footer-nav"
 import Header from "@/components/header"
@@ -17,6 +17,7 @@ export default function Main({
 }>) {
   const pathname = usePathname()
   const router = useRouter()
+  const [isHeaderVisible, setIsHeaderVisible] = useState(false)
 
   useEffect(() => {
     const PUBLIC_ROUTES = ["/login"]
@@ -26,8 +27,10 @@ export default function Main({
       try {
         const response = await AuthAPI.getSession()
         if (response.errors && !isPublic) {
+          setIsHeaderVisible(false)
           router.push("/login")
         } else {
+          setIsHeaderVisible(true)
           router.push(pathname)
         }
       } catch (error) {
@@ -45,14 +48,13 @@ export default function Main({
   return (
     <>
       <div className="hidden md:flex p-6 h-screen overflow-hidden m-auto max-w-[1232px]">
-        <Sidebar />
-        <div className="flex-1">
-            <Header />
+        {isHeaderVisible && <Sidebar />}
+        <div className="flex-1">{isHeaderVisible && <Header />}
           <div className="container mx-auto p-4">{children}</div>
         </div>
       </div>
       <div className="md:hidden container mx-auto p-4 h-[calc(100%-2rem)]">
-          <Header className="flex-shrink-0" />
+        {isHeaderVisible && <Header className="flex-shrink-0" />}
         <main className="flex-1 overflow-hidden">{children}</main>
         <MobileFooterNav className="flex-shrink-0" />
       </div>
