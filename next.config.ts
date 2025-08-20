f0a92719a82882af32073ecb3b139953e9742517
Farrah Mae Ochoa <farrah@deriv.com>
2025-08-20 14:47:42 +0400

    fix: cookie

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,17 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      if (Array.isArray(config.externals)) {
+        config.externals.push('cookie');
+      } else if (typeof config.externals === 'object') {
+        config.externals['cookie'] = 'commonjs cookie';
+      }
+    }
+    return config;
+  },
 }
 
 export default nextConfig
