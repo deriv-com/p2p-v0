1940c03de791ff7062bc4a20ad8c47824df409e7
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2025-11-05 11:50:47 +0400

    Revert "farrah/fix: datadog integration (#536)" (#537)
    This reverts commit 839f91118fa9d515c7e170e12ed9fe0794782c66.

--- components/datadog-rum-init.tsx
+++ components/datadog-rum-init.tsx
@@ -4,10 +4,7 @@ import { useEffect } from "react"
 import { initDatadog } from "@/lib/datadog"
 
 export function DatadogRumInit() {
-  console.log("[v0] DatadogRumInit component rendered")
-
   useEffect(() => {
-    console.log("[v0] DatadogRumInit useEffect running")
     initDatadog()
   }, [])
 
