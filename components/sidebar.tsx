e69ee550d967b1c4fffdad7ff0e5f6295b26d4b4
Farrah Mae Ochoa <farrah@deriv.com>
2026-02-16 08:40:16 +0400

    fix: revert sidebar changes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -20,6 +20,7 @@ import WalletSelectedIcon from "@/public/icons/ic-wallet-selected.svg"
 import ProfileIcon from "@/public/icons/profile-icon.svg"
 import ProfileSelectedIcon from "@/public/icons/profile-icon-red.svg"
 import GuideIcon from "@/public/icons/ic-guide.svg"
+import GuideSelectedIcon from "@/public/icons/ic-guide-selected.svg"
 import HomeIcon from "@/public/icons/ic-house.svg"
 import LiveChatIcon from "@/public/icons/ic-livechat.svg"
 
@@ -70,17 +71,17 @@ export default function Sidebar({ className }: SidebarProps) {
   const navItems = [
     ...(!isDisabled
       ? [
-          { name: t("navigation.home"), href: homeUrl, icon: HomeIcon, selectedIcon: HomeIcon },
-          { name: t("navigation.market"), href: "/", icon: MarketIcon, selectedIcon: MarketSelectedIcon },
-          { name: t("navigation.orders"), href: "/orders", icon: OrdersIcon, selectedIcon: OrdersSelectedIcon },
-          { name: t("navigation.myAds"), href: "/ads", icon: AdsIcon, selectedIcon: AdsSelectedIcon },
-          ...(showWallet
-            ? [{ name: t("navigation.wallet"), href: "/wallet", icon: WalletIcon, selectedIcon: WalletSelectedIcon }]
-            : []),
-          { name: t("navigation.profile"), href: "/profile", icon: ProfileIcon, selectedIcon: ProfileSelectedIcon },
-          { name: t("navigation.p2pHelpCentre"), href: helpCentreUrl, icon: GuideIcon, selectedIcon: GuideIcon },
-          { name: t("navigation.liveChat"), href: liveChatUrl, icon: LiveChatIcon, selectedIcon: LiveChatIcon },
-        ]
+        { name: t("navigation.home"), href: homeUrl, icon: HomeIcon, selectedIcon: HomeIcon },
+        { name: t("navigation.market"), href: "/", icon: MarketIcon, selectedIcon: MarketSelectedIcon },
+        { name: t("navigation.orders"), href: "/orders", icon: OrdersIcon, selectedIcon: OrdersSelectedIcon },
+        { name: t("navigation.myAds"), href: "/ads", icon: AdsIcon, selectedIcon: AdsSelectedIcon },
+        ...(showWallet
+          ? [{ name: t("navigation.wallet"), href: "/wallet", icon: WalletIcon, selectedIcon: WalletSelectedIcon }]
+          : []),
+        { name: t("navigation.profile"), href: "/profile", icon: ProfileIcon, selectedIcon: ProfileSelectedIcon },
+        { name: t("navigation.p2pHelpCentre"), href: helpCentreUrl, icon: GuideIcon, selectedIcon: GuideSelectedIcon },
+        { name: t("navigation.liveChat"), href: liveChatUrl, icon: LiveChatIcon, selectedIcon: LiveChatIcon },
+      ]
       : []),
   ]
 
