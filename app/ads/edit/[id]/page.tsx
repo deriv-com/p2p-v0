a663f05d8bbfcc971e1c8fc84bef0f162980eb43
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2026-05-06 17:29:05 +0400

    Revert "fix: remove edge runtime (#1106)" (#1113)
    This reverts commit 0923c9944028b5c363053b8d49410b055dd2326b.

--- app/ads/edit/[id]/page.tsx
+++ app/ads/edit/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const runtime = 'edge'
+
 import { useParams } from "next/navigation"
 import MultiStepAdForm from "@/app/ads/components/shared/multi-step-ad-form"
 
--- app/advertiser/[id]/page.tsx
+++ app/advertiser/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const runtime = "edge"
+
 import { useState, useEffect, useRef, useMemo, useCallback } from "react"
 import { useParams, useRouter, useSearchParams } from "next/navigation"
 import Image from "next/image"
