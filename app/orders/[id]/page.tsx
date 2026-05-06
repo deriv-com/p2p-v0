af6615eb2dad13d9c77c8c3c2f00730fff57674a
Farrah Mae Ochoa <82315152+farrah-deriv@users.noreply.github.com>
2026-05-06 17:28:51 +0400

    Revert "fix: remove edge runtime (#1106)"
    This reverts commit 0923c9944028b5c363053b8d49410b055dd2326b.

--- app/orders/[id]/page.tsx
+++ app/orders/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+export const runtime = "edge"
+
 import { useState, useEffect, useCallback } from "react"
 import { useParams } from 'next/navigation'
 import { ChevronRight } from 'lucide-react'
